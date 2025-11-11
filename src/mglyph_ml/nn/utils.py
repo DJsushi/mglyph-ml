16d581125f64b05f2bbca559b91ef69b66f28e66
Martin Gaens <djsushi123@gmail.com>
2025-11-11 14:29:28 +0000

    ref: move train_one_epoch to separate file

---
+++ src/mglyph_ml/nn/utils.py
@@ -0,0 +1,34 @@
+from torch import nn
+from torch.utils.data import DataLoader
+
+def train_one_epoch(
+    model: nn.Module,
+    train_data_loader: DataLoader,
+    device: str,
+    criterion,  # unfortunately, has no supertype... There's torch.nn.modules._Loss, but it's private
+    optimizer,  # same as above
+) -> float:
+    model.train()
+    running_loss = 0.0
+    num_batches = 0
+
+    for index, data in enumerate(train_data_loader):
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        labels = labels.view(-1, 1)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        num_batches += 1
+        # Optionally print every N batches
+        # if (i + 1) % 10 == 0:
+        # print(f"[Epoch {epoch+1}, Batch {i+1}] loss: {loss.item():.4f}")
+
+    avg_loss = running_loss / num_batches if num_batches > 0 else 0.0
+    return avg_loss
